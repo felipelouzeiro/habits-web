1200dc6a9d1efbafc13ead0a37d878537dc95c0f
Felipe Alves Louzeiro <fl.louzeiro@gmail.com>
2023-02-26 23:13:45 -0300

    feat: alimentando grade com dados da api

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -3,13 +3,18 @@ import * as Checkbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
 import { Check } from 'phosphor-react';
+import dayjs from 'dayjs';
 interface HabitDayProps {
-  amount: number;
-  completed: number;
+  date: Date;
+  amount?: number;
+  completed?: number;
 }
 
-export function HabitDay({ amount, completed }: HabitDayProps) {
-  const completedPercentage = (completed / amount) * 100;
+export function HabitDay({ amount = 0, completed = 0, date }: HabitDayProps) {
+  const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0;
+
+  const dayAndMonth = dayjs(date).format('DD/MM');
+  const dayOfWeek = dayjs(date).format('dddd');
   return (
     <Popover.Root>
       <Popover.Trigger
@@ -24,8 +29,8 @@ export function HabitDay({ amount, completed }: HabitDayProps) {
       ></Popover.Trigger>
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-          <span className="font-semibold text-zinc-400">terça-feira</span>
-          <span className="mt-1 font-extrabold leading-tight text-3xl">17/01</span>
+          <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
           <ProgressBar progress={completedPercentage} />
 
           <div className="mt-6 flex flex-col gap-3">
--- src/components/SumaryTable.tsx
+++ src/components/SumaryTable.tsx
@@ -1,4 +1,6 @@
-import { Plus } from 'phosphor-react';
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+import { api } from '../lib/axios';
 import { generateDatesFromYearBeginning } from '../utils/generate-days-from-year-beginning';
 import { HabitDay } from './HabitDay';
 
@@ -8,7 +10,20 @@ const summaryDates = generateDatesFromYearBeginning();
 const minimumSummaryDatesSize = 18 * 7; //18 weeks
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
+type Summary = Array<{
+  id: number;
+  date: string;
+  amount: number;
+  completed: number;
+}>;
+
 export function SummaryTable() {
+  const [summary, setSummary] = useState<Summary>([]);
+
+  useEffect(() => {
+    api.get('/summary').then((response) => setSummary(response.data));
+  }, []);
+
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -25,8 +40,16 @@ export function SummaryTable() {
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
+          const dayInSummary = summary.find((day) => {
+            return dayjs(date).isSame(day.date, 'day');
+          });
           return (
-            <HabitDay key={date.toString()} amount={5} completed={Math.round(Math.random() * 5)} />
+            <HabitDay
+              key={date.toString()}
+              date={date}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
+            />
           );
         })}
         {amountOfDaysToFill > 0 &&
