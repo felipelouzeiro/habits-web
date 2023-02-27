4bdb865c357a5b80c7b754a60ba7bc71c0949629
Felipe Alves Louzeiro <fl.louzeiro@gmail.com>
2023-02-27 00:08:56 -0300

    feat: implementando check/uncheck de hábitos

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,9 +1,8 @@
 import * as Popover from '@radix-ui/react-popover';
-import * as Checkbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
-import { Check } from 'phosphor-react';
 import dayjs from 'dayjs';
+import { HabitsList } from './HabitList';
 interface HabitDayProps {
   date: Date;
   amount?: number;
@@ -15,6 +14,9 @@ export function HabitDay({ amount = 0, completed = 0, date }: HabitDayProps) {
 
   const dayAndMonth = dayjs(date).format('DD/MM');
   const dayOfWeek = dayjs(date).format('dddd');
+
+  function handleCompletedChange() {}
+
   return (
     <Popover.Root>
       <Popover.Trigger
@@ -32,19 +34,7 @@ export function HabitDay({ amount = 0, completed = 0, date }: HabitDayProps) {
           <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
           <ProgressBar progress={completedPercentage} />
-
-          <div className="mt-6 flex flex-col gap-3">
-            <Checkbox.Root className="flex items-center gap-3 group">
-              <div className="h-8 w-8 flex rounded-lg items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
-              <span className=" font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-          </div>
+          <HabitsList date={date} />
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
       </Popover.Portal>
---
+++ src/components/HabitList.tsx
@@ -0,0 +1,75 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
+import dayjs from 'dayjs';
+import { Check } from 'phosphor-react';
+import { useEffect, useState } from 'react';
+import { api } from '../lib/axios';
+
+interface HabitListProps {
+  date: Date;
+}
+
+interface HabitInfo {
+  possibleHabits: Array<{ id: string; title: string; created_at: string }>;
+  completedHabits: string[];
+}
+
+export function HabitsList({ date }: HabitListProps) {
+  const [habitsInfo, setHabitsInfo] = useState<HabitInfo>();
+
+  useEffect(() => {
+    api
+      .get('/day', { params: { date: date.toISOString() } })
+      .then((response) => setHabitsInfo(response.data));
+  }, []);
+
+  const isDateInPaste = dayjs(date).endOf('day').isBefore(new Date());
+
+  async function handleToggleHabit(habitId: string) {
+    await api.patch(`/habits/${habitId}/toggle`);
+
+    const isHabitAlreadyCompleted = habitsInfo?.completedHabits.includes(habitId);
+
+    let completedHabits: string[] = [];
+
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter((id) => id !== habitId);
+
+      setHabitsInfo({
+        possibleHabits: habitsInfo!.possibleHabits,
+        completedHabits,
+      });
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId];
+
+      setHabitsInfo({
+        possibleHabits: habitsInfo!.possibleHabits,
+        completedHabits,
+      });
+    }
+  }
+
+  return (
+    <div className="mt-6 flex flex-col gap-3">
+      {habitsInfo?.possibleHabits.map((habit) => {
+        return (
+          <Checkbox.Root
+            key={habit.id}
+            onCheckedChange={() => handleToggleHabit(habit.id)}
+            checked={habitsInfo.completedHabits.includes(habit.id)}
+            disabled={isDateInPaste}
+            className="flex items-center gap-3 group"
+          >
+            <div className="h-8 w-8 flex rounded-lg items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+            <span className=" font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+              {habit.title}
+            </span>
+          </Checkbox.Root>
+        );
+      })}
+    </div>
+  );
+}
